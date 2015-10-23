7a730b2962714744cfafc6e01d16c02dc03187e9
Agustin Romano <agustinr88@gmail.com>
2015-10-22 21:13:52 -0300

    Implementacion basica de filtro de usuarios para ser utilizado por lista desplegable en la vista

--- model/users.js
+++ model/users.js
@@ -18,5 +18,32 @@ var Users = {
 		}
 		xhttp.open("GET", "/data/users.json", true);
 		xhttp.send();
+	},
+	
+	getMatchingPeople : function (inputPref, callback) {
+		inputPref.toLowerCase();
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (xhttp.readyState == 4 && xhttp.status == 200) {
+			  var json = JSON.parse(xhttp.responseText);
+			  var users = [];
+			  for(i=0; i<json.length;i++) {
+				  var curPerson = json[i];
+				  users.push(curPerson.username);
+			  }
+			  var n = inputPref.length;
+			  var selectedUsers = [];
+			  for(i=0; i<users.length;i++) {
+				  var curUser = users[i];
+				  var curUserPref = curUser.substring(0, n).toLowerCase();
+				  if (inputPref == curUserPref) {
+					  selectedUsers.push(curUser);
+				  }
+			  }
+			  callback(selectedUsers);
+			} //TODO else?
+		}
+		xhttp.open("GET", "/data/users.json", true);
+		xhttp.send();	
 	}
 }
